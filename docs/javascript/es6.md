1b8a7bb600593a9167fbacdea781e59b76c620fe
张洋斌 <253626232@qq.com>
2019-01-09 21:29:31 +0800

    update Set & Map

--- docs/javascript/es6.md
+++ docs/javascript/es6.md
@@ -1,4 +1,39 @@
 # ES6
 ## Set & Map
-   * ES6 提供了新的数据结构 Set。它类似于数组，但是成员的值都是唯一的，没有重复的值  
-   [Set & Map](https://blog.csdn.net/chauncywu/article/details/73302353)
+### Set 
+   + ES6 提供了新的数据结构 Set。它类似于数组，但是成员的值都是唯一的，没有重复的值   
+   + new Set(params) params可选，且必须是array 
+   + Set实例的本质是一个数组，可以调用数组的方法  
+   + Set方法最大的方便就是可以去重基本数据类型组成的数组
+```
+const arr = new Set([1,2,3,4,5,1,2])
+console.log(arr) // => [1,2,3,4,5]
+const friends = new Set([
+        {name:"sunwukong",age:500},
+        {name:"zhubajie",age:600},
+        {name:"shaheshang",age:700},
+        {name:"sunwukong",age:500}
+    ])
+console.log(friends.length) // => 4 引用数据类型无法去重  
+```
+   + Set实例方法
+      1. add(value) ->添加某个值，返回Set结构本身
+      2. delete(value) ->删除某个值，返回一个布尔值，表示删除是否成功
+      3. has(value) ->返回一个布尔值，表示该值是否为Set的成员
+      4. clear() ->清除所有成员，没有返回值
+```
+注意：has & delete 无法操作引用数据类型
+```
+### Map
+  * ES6 提供了 Map 数据结构。它类似于对象，也是键值对的集合，但是“键”的范围不限于字符串，各种类型   的值（包括对象）都可以当作键
+  * Map构造函数接受数组作为参数
+  ```
+  const items = [
+    ['name', '张三'],
+    ['title', 'Author']
+  ]
+  const map = new Map();
+  items.forEach(
+    ([key, value]) => map.set(key, value)
+  )
+  ```
--- docs/javascript/test.js
+++ docs/javascript/test.js
@@ -1,73 +1,23 @@
-const westTeam = ['tansheng','sunwukong','zhubajie','shaheshang','bailongma']
-function test(item){
-    if(item=='sunwukong' || item=='zhubajie'){
-        console.log('in')
-    }else{
-        console.log('out')
-    }
-}
-test('sunwukong')
+const arr = new Set([1,2,3,4,5,1,2])
+console.log(arr)
 {
-    function test(name,age = 500){
-        console.log('my nama is ' + name,'my age is ' + age)
-    }
-    test('sunwukong')
-    test('zhubajie',800)
-}
-{
-    function test({ name } = { }){
-        console.log('my name is ' + (name || 'unknow'))
-    }
-    test('sunwukong')
-    test()
-    test({name:'sunwukong',age:500})
+    const friends = new Set([
+        {name:"sunwukong",age:500},
+        {name:"zhubajie",age:600},
+        {name:"shaheshang",age:700},
+        {name:"sunwukong",age:500}
+    ])
+    console.log(friends) 
+    const result = friends.delete({name:'sunwukong',age:500})
+    console.log(result)
+    console.log('-------------')
+    console.log(friends.entries())
+    console.log('-------------')
 }
 {
-    const teamers = [
-        {name:'tansheng',age:24},
-        {name:'sunwukong',age:500},
-        {name:'zhubajie',age:600},
-        {name:'shashidi',age:700}
-    ]
-    function test(name = 'tansheng'){
-        switch(name){
-            case 'sunwukong':
-                return 500
-            case 'zhubajie':
-                return 600
-            case 'shashidi':
-                return 700
-            default:
-                return 24
-        }
-    }
-    test('sunwukong')
-}
-{
-    const teamers = [
-        {name:'tansheng',age:24},
-        {name:'sunwukong',age:500},
-        {name:'zhubajie',age:600},
-        {name:'shashidi',age:700}
-    ]
-    function test(name = 'tansheng'){
-        for(let item of teamers){
-            if(item.name==name){
-                console.log(item['age'])
-            }
-        }
-    }
-    test('sunwukong')
-    test()
-}
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-{
-    function test(name){
-        const westTeam = ['tansheng','sunwukong','zhubajie','shaheshang','bailongma']
-        const has = westTeam.some(item => item==name)
-        const all = westTeam.every(item => item==name)
-        console.log(has, all)
-    }
-    test('')
-    test('sunwukong') 
+    let obj = new Set([{
+        name:'swk',
+        age:500
+    }])
+    console.log(obj)
 }
